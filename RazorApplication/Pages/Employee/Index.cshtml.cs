acb6dd1fb67173d5b3f7d24e91ec603843097db4
SagarJaybhay <jaybhay.sagar18@gmail.com>
2020-03-14 20:24:43 +0530

    changes

---
+++ RazorApplication/Pages/Employee/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApplication.Pages.Employee
+{
+    public class IndexModel : PageModel
+    {
+        public List<RazorPages.Models.Employee> Employees;
+        public IndexModel(IEmployeeRepos employeeRepos)
+        {
+            EmployeeRepos = employeeRepos;
+        }
+
+        public IEmployeeRepos EmployeeRepos { get; }
+
+        public void OnGet()
+        {
+            Employees = EmployeeRepos.GetAllEmployees().ToList();          
+        }
+    }
+}
---
+++ RazorApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string Message { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "Hello Mr. Sagar Jaybhay.";
+        }
+    }
+}
